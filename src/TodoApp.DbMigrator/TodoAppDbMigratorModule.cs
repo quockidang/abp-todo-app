ddd40e36099458fd7d55096a9a1c81ceb41b58cb
Dang Quoc Ki <ki.dang@ndsvn.vn>
2024-11-29 12:09:33 +0700

    init project

---
+++ src/TodoApp.DbMigrator/TodoAppDbMigratorModule.cs
@@ -0,0 +1,21 @@
+﻿using TodoApp.EntityFrameworkCore;
+using Volo.Abp.Autofac;
+using Volo.Abp.Caching;
+using Volo.Abp.Caching.StackExchangeRedis;
+using Volo.Abp.Modularity;
+
+namespace TodoApp.DbMigrator;
+
+[DependsOn(
+    typeof(AbpAutofacModule),
+    typeof(AbpCachingStackExchangeRedisModule),
+    typeof(TodoAppEntityFrameworkCoreModule),
+    typeof(TodoAppApplicationContractsModule)
+    )]
+public class TodoAppDbMigratorModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        Configure<AbpDistributedCacheOptions>(options => { options.KeyPrefix = "TodoApp:"; });
+    }
+}
